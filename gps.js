f8ff8d3bfeed2b330d60de31b07c3adef3acc8a3
D1ego419 <diego.villablanca1@virginiogomez.cl>
2026-06-06 19:09:35 -0400

    Actualiza unidad gps

---
+++ gps.js
@@ -0,0 +1,50 @@
+import { app } from "./firebase.js";
+
+import {
+    getDatabase,
+    ref,
+    update
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+
+const db = getDatabase(app);
+
+document
+.getElementById("btnGPS")
+.addEventListener("click", () => {
+
+    const unidad =
+    document.getElementById("unidad").value;
+
+    navigator.geolocation.watchPosition(
+
+        (position) => {
+
+            update(
+                ref(db, "unidades/" + unidad),
+                {
+                    estado: "En Ruta",
+
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+            );
+
+            console.log(
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+        },
+
+        (error) => {
+            alert("GPS no disponible");
+        },
+
+        {
+            enableHighAccuracy: true
+        }
+
+    );
+
+});
